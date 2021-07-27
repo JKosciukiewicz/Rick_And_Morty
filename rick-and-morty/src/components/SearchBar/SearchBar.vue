284d66636798054044f1ad4717c18bf1a442708b
KboRodo <61941560+KboRodo@users.noreply.github.com>
2021-07-27 20:30:20 +0200

    added vuex

---
+++ rick-and-morty/src/components/SearchBar/SearchBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="SearchBar">
+      <h1>RICK AND MORTY</h1>
+      <input type="text"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  props: {
+    msg: String
+  }
+}
+</script>
