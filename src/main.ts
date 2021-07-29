0dde25c585a38a6f773e936c96549d39a1d91f36
KboRodo <61941560+KboRodo@users.noreply.github.com>
2021-07-29 23:35:38 +0200

    added apollo

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,39 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { ApolloClient, createHttpLink, InMemoryCache, gql } from '@apollo/client/core'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+    // You should use an absolute URL here
+    uri: 'https://rickandmortyapi.com/graphql',
+  })
+  
+  // Cache implementation
+  const cache = new InMemoryCache()
+
+  const query=gql`
+        query {
+        characters(page: 1, filter: { name: "rick" }) {
+            info {
+            count
+            }
+            results {
+            name,
+            species,
+            gender,
+            }
+        }
+    }
+  `
+  
+  // Create the apollo client
+  const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+  })
+
+  apolloClient.query({
+      query
+  }).then(res=>console.log(res))
 
 createApp(App).mount('#app')
