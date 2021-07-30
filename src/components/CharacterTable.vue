4881fb9430d963bb3dec30d9d23221cb9b935c08
KboRodo <61941560+KboRodo@users.noreply.github.com>
2021-07-30 18:05:13 +0200

    finished display

--- src/components/CharacterTable.vue
+++ src/components/CharacterTable.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="characterTable">
       <div class="optionsMenu"></div>
-      <div class="characterList1">
-          <ul>
-              <li v-for="character in characters" :key="character">
+      <div class="characterList">
+        <div class="characterList__header">
+          <p>Photo</p>
+          <p>CharacterID</p>
+          <p>Name</p>
+          <p>Species</p>
+          <p>Gender</p>
+          <p>Last Episode</p>
+          <p>Add To Favorite</p>
+        </div>
+          <ul class="characterList__list">
+              <li v-for="character in characters" :key="character" class="characterList__list__element">
                 <img :src=character.image>
                 <p>{{character.id}}</p>
                 <p>{{character.name}}</p>
                 <p>{{character.species}}</p>
                 <p>{{character.gender}}</p>
+                <p>{{character.episode[character.episode.length -1].episode}}</p>
+                <p>Add to Favorite</p>
               </li>
           </ul>
       </div>
