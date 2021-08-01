7ac7e0d7e247dc99548b8242d92c7a5c42c8c9cb
KboRodo <61941560+KboRodo@users.noreply.github.com>
2021-08-02 00:39:54 +0200

    rollback

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { createApp } from 'vue'
+import { createApp, provide, render,h } from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
 import apolloClient from './apollo/apollo'
 import { DefaultApolloClient } from '@vue/apollo-composable'
-import store from './store/index'
-=======
 import store from './store'
->>>>>>> f884c40f836496b1cd2efd0ae09333a218ef8ca4
 
-createApp(App).use(store).mount('#app')
+
+createApp({
+  setup(){
+    provide(DefaultApolloClient, apolloClient)
+  },
+  render(){
+    return h(App)
+  }
+}).use(store).mount('#app')
